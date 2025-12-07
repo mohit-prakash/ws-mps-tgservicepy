b6113f0d77301bd0b1819bb278d9aa45d69b857c
Mohit Prakash <m2450p@gmail.com>
2025-12-07 22:48:57 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # quick runner for local development
 export $(grep -v '^#' .env | xargs)
-uvicorn app.main:app --reload --port 9000
+uvicorn app.main:app --reload --host 0.0.0.0 --port 9000
 #uvicorn app.main:app --port 9000
